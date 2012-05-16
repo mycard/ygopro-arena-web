79f642db73e31bbca831cf51571b0793a8f2b54a
zh99998 <zh99998@gmail.com>
2012-05-16 20:43:02 +0800

    session存活期 9999years

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MycardServerHttp::Application.config.session_store :cookie_store, key: '_mycard-server-http_session'
+MycardServerHttp::Application.config.session_store :cookie_store, key: '_mycard-server-http_session', :expire_after => 9999.years
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
